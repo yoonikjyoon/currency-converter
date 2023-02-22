fbff5b1bb6fa9735ec71f2ae4c48934b716dbd6e
Yooni <yooni.kj.yoon@gmail.com>
2023-02-22 17:38:25 +0900

    added color constant

---
+++ src/constants/colors.js
@@ -0,0 +1,10 @@
+export const colors = {
+  white: "#ffffff",
+  black: "#000000",
+  coolGray: "#b3b3bc",
+  paleGray03: "#e0e0e5",
+  paleGray02: "#f1f1f3",
+  paleGray01: "#f9fafb",
+  dark: "#24242c",
+  lavender: "#7577c7",
+};
