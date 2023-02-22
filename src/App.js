fbdab6309f3280ef4c5e08dce60c0275be372e05
Yooni <yooni.kj.yoon@gmail.com>
2023-02-22 17:33:53 +0900

    modified styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import { getCurrency } from "./api/currency";
-import "./App.css";
+import InputText from "./components/InputText";
+import styled from "styled-components";
+import { colors } from "./constants/colors";
 
 function App() {
   return (
-    <>
-      <div className="app-container">
-        <div className="converter-container" onClick={() => getCurrency()}>
-          Currency Converter
-        </div>
-      </div>
-    </>
+    <Container>
+      <MainWrap>
+        <InputText />
+      </MainWrap>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  position: fixed;
+  width: 100vw;
+  height: 100%;
+  background-color: ${colors.lavender};
+`;
+const MainWrap = styled.div`
+  position: absolute;
+  background-color: ${colors.white};
+  width: 80vw;
+  height: 50vh;
+  max-width: 420px;
+  min-width: 320px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: 20px;
+  padding: 30px;
+`;
