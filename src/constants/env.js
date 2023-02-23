0cead78a9c78804d469bbc2731a960ba0a88c586
Yooni <yooni.kj.yoon@gmail.com>
2023-02-23 16:26:16 +0900

    added default values

---
+++ src/constants/env.js
@@ -0,0 +1,10 @@
+export const DEFAULT_FROM_VALUE = {
+  code: "USD",
+  description: "United States",
+  amount: 1,
+};
+export const DEFAULT_TO_VALUE = {
+  code: "KRW",
+  description: "South Korean",
+  amount: null,
+};
